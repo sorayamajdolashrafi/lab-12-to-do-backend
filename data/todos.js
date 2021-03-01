7dce54512dfd7d1f367282ddccc967ae57b799f3
Soraya Benson <soraya.benson@gmail.com>
2021-03-01 15:21:19 -0800

    todos data and endpoint working

--- data/todos.js
+++ data/todos.js
@@ -1,7 +1,16 @@
 module.exports = [
   {
-    hacer: '',
+    id: 1,
+    hacer: 'prueba',
+    color: 'amarillo',
     completed: false,
-    user_id: 1,
+    user_id: 1
   },
+  {
+    id: 2,
+    hacer: 'prueba dos',
+    color: 'azul',
+    completed: false,
+    user_id: 1
+  }
 ];
