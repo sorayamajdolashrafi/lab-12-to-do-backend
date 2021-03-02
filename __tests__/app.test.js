08ff07709ec8afb4567e97c913b5c98cf27428f5
Soraya Benson <soraya.benson@gmail.com>
2021-03-01 17:31:53 -0800

    tests written and passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,35 +31,176 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    const todo = {
+      hacer: 'test time',
+      color: 'red',
+      completed: false,
+    };
+
+    const databaseTodos = {
+      ...todo,
+      user_id: 2,
+      id: 7
+    };
+
+    test('post endpoint to create a todo', async() => {
+
+      const todo = {
+        hacer: 'test time',
+        color: 'red',
+        completed: false,
+      };
+
+      const data = await fakeRequest(app)
+        .post('/api/todos')
+        .send(todo)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(databaseTodos);
+    });
+
+    test('put endpoint to update a todo', async() => {
+
+      const updatedTodo = {
+        hacer: 'updated test time',
+        color: 'pink',
+        completed: true,
+      };
+
+      await fakeRequest(app)
+        .put('/api/todos/7')
+        .send(updatedTodo)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const updatedData = await fakeRequest(app)
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const updatedDatabaseTodos = [
+        {
+          hacer: 'updated test time',
+          color: 'pink',
+          completed: true,
+          id: 7,
+          user_id: 2
+        }
+      ];
+
+      expect(updatedData.body).toEqual(updatedDatabaseTodos);
+    });
+
+    test('get endpoint returns a specific user`s todos', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          hacer: 'updated test time',
+          color: 'pink',
+          completed: true,
+          id: 7,
+          user_id: 2
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/api/todos')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
     });
+
+    test('get endpoint returns all todos', async() => {
+
+      const expectation = [
+        {
+          id: 1,
+          hacer: 'tarea',
+          color: 'amarillo',
+          completed: false,
+          user_id: 1
+        },
+        {
+          id: 2,
+          hacer: 'call my dad',
+          color: 'verde',
+          completed: false,
+          user_id: 1
+        },
+        {
+          id: 3,
+          hacer: 'snack on chocolate',
+          color: 'rosado',
+          completed: false,
+          user_id: 1
+        },
+        {
+          id: 4,
+          hacer: 'call charlie',
+          color: 'verde',
+          completed: false,
+          user_id: 1
+        },
+        {
+          id: 5,
+          hacer: 'cena',
+          color: 'amarillo',
+          completed: false,
+          user_id: 1
+        },
+        {
+          id: 6,
+          hacer: 'tecito',
+          color: 'rosado',
+          completed: false,
+          user_id: 1
+        },
+        {
+          id: 7,
+          hacer: 'updated test time',
+          color: 'pink',
+          completed: true,
+          user_id: 2
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/todos')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    // This test cannot pass because a new hash for user 2 is created every time the test runs.
+
+    // test('get endpoint returns users', async() => {
+
+    //   const expectation = [
+    //     {
+    //       id: 1,
+    //       email: 'john@arbuckle.com',
+    //       hash: '1234'
+    //     },
+    //     {
+    //       id: 2,
+    //       email: 'jon@user.com',
+    //       hash: '$2a$08$bC.ONg7ldx5RMO/52yYvguvjcbOt6X3JoCUEueSRw/G/1ct3P2Fzi'
+    //     }
+    //   ];
+
+    //   const data = await fakeRequest(app)
+    //     .get('/users')
+    //     .expect('Content-Type', /json/)
+    //     .expect(200);
+
+    //   expect(data.body).toEqual(expectation);
+    // });
   });
 });
