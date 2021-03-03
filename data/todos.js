e817f80be7b9aa6d139c1228ef030465b0a37841
Soraya Benson <soraya.benson@gmail.com>
2021-03-03 00:44:16 -0800

    updated endpoints tests need updated

--- data/todos.js
+++ data/todos.js
@@ -16,23 +16,5 @@ module.exports = [
     color: 'rosado',
     completed: false,
     user_id: 2
-  },
-  {
-    hacer: 'call charlie',
-    color: 'verde',
-    completed: false,
-    user_id: 2
-  },
-  {
-    hacer: 'cena',
-    color: 'amarillo',
-    completed: false,
-    user_id: 1
-  },
-  {
-    hacer: 'tecito',
-    color: 'rosado',
-    completed: false,
-    user_id: 1
   }
 ];
